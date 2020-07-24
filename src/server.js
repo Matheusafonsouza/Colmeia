07f5ca79d7aa8acee8e2a3105f5abfe75eef97eb
matheusafonsouza <matheusafonsouza@gmail.com>
2020-07-24 16:17:40 -0300

    Created a function for the planet with the lower lifespan

--- src/server.js
+++ src/server.js
@@ -10,4 +10,16 @@ async function firstMovie() {
   console.log(result.get('title'));
 }
 
+async function lifeSpan() {
+  const query = new app.Query('Specie');
+
+  query.exists('averageLifespan');
+  query.ascending('averageLifespan');
+  query.limit(3);
+
+  const results = await query.find();
+
+  console.log(results);
+}
+
 firstMovie();
